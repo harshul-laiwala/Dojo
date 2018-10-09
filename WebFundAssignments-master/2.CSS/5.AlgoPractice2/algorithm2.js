ce822465a8df4b62e8c69542410470a1934725e5
harshul-laiwala <harshul.laiwala@gmail.com>
2018-10-08 21:53:59 -0700

    Update WebFundAssignments-master/2.CSS/5.AlgoPractice2/algorithm2.js

--- WebFundAssignments-master/2.CSS/5.AlgoPractice2/algorithm2.js
+++ WebFundAssignments-master/2.CSS/5.AlgoPractice2/algorithm2.js
@@ -1,8 +1,33 @@
 // this is part 2 of the assignment. Copy/paste the starter code from the platform like seen below 
 //and edit the code to make it work.
 function printUpTo(x){
-    // your code here
+    if(x<0){
+        return false;
+    }
+    for(i=0;i<=x;i++) {
+        console.log(i);
+    }
 }
-printUpTo(1000000); // should print all the integers from 1 to 1000000
+console.log(printUpTo(10)); // should print all the integers from 1 to 1000000
 y = printUpTo(-10); // should return false
 console.log(y); // should print false
+
+function printSum(x){
+    var sum = 0;
+    for(i=0;i<=x;i++){
+        sum+=i;
+    }
+    return sum
+  }
+  y = printSum(255) // should print all the integers from 0 to 255 and with each integer print the sum so far.
+  console.log(y) // should print 32640
+
+  function printSumArray(x){
+    var sum = 0;
+    for(var i=0; i<x.length; i++) {
+      //your code here
+    }
+    return sum;
+  }
+  console.log( printSumArray([1,2,3]) ); // should log 6
+
