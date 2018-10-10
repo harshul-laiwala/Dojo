e11e1de6a3b2e6f27fdaa37baaa9440fe7442699
harshul-laiwala <harshul.laiwala@gmail.com>
2018-10-10 02:10:35 -0700

    Update WebFundAssignments-master/2.CSS/8.AlgoPractice4/algorithm4.js

---
+++ WebFundAssignments-master/2.CSS/8.AlgoPractice4/algorithm4.js
@@ -0,0 +1,56 @@
+function GreaterThanValue(x,y){
+    var count=0;
+    for(i=0;i<x.length;i++){
+        if(x[i]>y){
+            count+=1;
+            console.log(x[i]);
+        }
+    }
+    console.log("total number of values greater than " + y + " are " + count);
+}
+GreaterThanValue([1,2,3,4],1);
+
+function MaxMinAvgValues(x){
+    var maxValue=0;
+    var minValue=0;
+    var avgValue=0;
+    var sum=0;
+    for(i=0;i<=x.length;i++){
+        if(x[i]>maxValue){
+            maxValue=x[i];
+        }
+        if(x[i]<minValue){
+            minValue=x[i];
+        }
+        sum+=x[i];   
+    }
+    avgValue=sum/x.length;
+    console.log("The maximum value in the array is " +maxValue);
+    console.log("The minimum value in the array is "+minValue);
+    console.log("The average of the array is "+avgValue);
+    console.log("The sum of the array is "+sum);
+}
+MaxMinAvgValues([1,2,3,4]);
+
+function replaceNegatives(x){
+    var newarr=[];
+    for(i=0;i<x.length;i++){
+        if(x[i]<0){
+            newarr[i]="Dojo";
+        }else newarr[i]=x[i];
+    }
+    console.log(newarr);
+}
+replaceNegatives([1,-1,-2]);
+
+function removeVals(x,startindex,endindex){
+    var newarr=[];
+    for(i=0;i<startindex;i++){
+        newarr.push(x[i]);
+    }
+    for(i=x.length-1;i>endindex;i--){
+        newarr.push(x[i]);
+    }
+    console.log(newarr);
+}
+removeVals([20,30,40,50,60,70],2,2);
