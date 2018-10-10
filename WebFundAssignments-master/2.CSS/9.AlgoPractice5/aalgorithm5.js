0c0e87c7057517ef310190c2c35e7cbea729634f
harshul-laiwala <harshul.laiwala@gmail.com>
2018-10-10 15:16:12 -0700

    Update WebFundAssignments-master/2.CSS/9.AlgoPractice5/aalgorithm5.js

--- WebFundAssignments-master/2.CSS/9.AlgoPractice5/aalgorithm5.js
+++ WebFundAssignments-master/2.CSS/9.AlgoPractice5/aalgorithm5.js
@@ -11,14 +11,32 @@ resetNegatives( [1,2,-1, -3]);
 
 //Challenge 2
 function moveForward(x){
-    var newarr = [];
-    for(i=x.length-1;i>0;i--){
-        
+    for(i=0;i<x.length;i++){
+        x[i] = x[i+1];
     }
-
-
+    x[x.length-1]=0;
+    console.log(x);
 }
-moveForward( [1,2,3]);
+moveForward([1,2,3,4]);
 //Challenge 3
-
+function returnReversed(x){
+    reverseArr=[];
+    counter =0;
+    for(i=x.length-1;i>=0;i--){
+        reverseArr[counter] = x[i];
+        counter++;
+    }
+    console.log(reverseArr);
+}
+returnReversed([1,2,3,4]);
 //Challenge 4
+function repeatTwice(arr){
+    newArr=[];
+    for(i=0;i<arr.length;i++){
+        for(j=0;j<2;j++){
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr);
+}
+repeatTwice([4,"Ulysses",42,false]);
