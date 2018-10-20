7e40967a887cf0a724e447d263777c120402bca1
harshul-laiwala <harshul.laiwala@gmail.com>
2018-10-20 01:56:07 -0700

    Update WebFundAssignments-master/3.Javascript/4.PlayingWithObjects/PlayingWithObjects.js

---
+++ WebFundAssignments-master/3.Javascript/4.PlayingWithObjects/PlayingWithObjects.js
@@ -0,0 +1,17 @@
+// Objectives
+// Practice iterating through an array of objects/dictionaries.
+// Imagine that you are given an array of objects.  For example,
+// var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+// How would you print/log John's age?
+// How would you print/log the name of the first object?
+// How would you print/log the name and age of each user using a for loop?  Your output should look something like this
+// Michael - 37
+// John - 30
+// David - 27
+
+var users = [
+    {name: "Michael", age:37}, 
+    {name: "John", age:30}, 
+    {name: "David", age:27}
+];
+console.log(user.name)
