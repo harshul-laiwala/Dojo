a1518418f30dbfb3633af050392692a365aa4ff3
harshul-laiwala <harshul.laiwala@gmail.com>
2018-10-28 00:26:46 -0700

    intermediateFoundation assignment

--- WebFundAssignments-master/3.Javascript/1.BasicFunction1/functions.js
+++ WebFundAssignments-master/3.Javascript/1.BasicFunction1/functions.js
@@ -1,4 +1,4 @@
-function a(){
+function a() {
     return 35;
 }
 console.log(a())
@@ -6,38 +6,37 @@ console.log(a())
 //Prediction:a=35
 //Output: 35
 
-function a(){
+function a() {
     return 4;
 }
-console.log(a()+a());
+console.log(a() + a());
 //Prediction:a=4+a=4
 //Output:8
 
 
-function a(b){
+function a(b) {
     return b;
 }
-console.log(a(2)+a(4));
+console.log(a(2) + a(4));
 
 //Prediction: a=2 +a=4
 //Output: 6
 
 
-function a(b){
-        console.log(b);
-        return b*3;
-    }
-    console.log(a(3));
+function a(b) {
+    console.log(b);
+    return b * 3;
+}
+console.log(a(3));
 
 //Prediction: b=3 , b=9
 //Output: 3 , 9
 
 
-function a(b){
-    if(b<10) {
+function a(b) {
+    if (b < 10) {
         return 2;
-    }
-    else     {
+    } else {
         return 4;
     }
     console.log(b);
@@ -47,17 +46,17 @@ console.log(a(15));
 //Output: 4 
 
 
-function a(b,c){
-    return b*c;
+function a(b, c) {
+    return b * c;
 }
-console.log(10,3);
-console.log( a(3,10) );
+console.log(10, 3);
+console.log(a(3, 10));
 //Prediction:b=10, c=3 ; b=3, c=10
 //Output: 30, 30
 
 
-function a(b){
-    for(var i=0; i<10; i++){
+function a(b) {
+    for (var i = 0; i < 10; i++) {
         console.log(i);
     }
     return i;
@@ -68,9 +67,9 @@ console.log(4);
 //Output:0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9
 
 
-function a(){
-    for(var i=0; i<10; i++){
-        i = i +2;
+function a() {
+    for (var i = 0; i < 10; i++) {
+        i = i + 2;
         console.log(i);
     }
 }
@@ -79,21 +78,21 @@ a();
 //Output: 0,2,4,8
 
 
-function a(b,c){
-    for(var i=b; i<c; i++) {
-       console.log(i);
-   }
-   return b*c;
+function a(b, c) {
+    for (var i = b; i < c; i++) {
+        console.log(i);
+    }
+    return b * c;
 }
-a(0,10);
-console.log(a(0,10));
+a(0, 10);
+console.log(a(0, 10));
 //Prediction:b=0,c=10 ,i=0;1;2;3;4;5;6;7;8;9;10
 //Output:0,1,2,3,4,5,7,8,9,0,1,2,3,4,5,7,8,9,0
 
 
-function a(){
-    for(var i=0; i<10; i++){
-        for(var j=0; j<10; j++){
+function a() {
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
             console.log(j);
         }
         console.log(i);
@@ -103,12 +102,12 @@ function a(){
 //Output: no output since function is never called 
 
 
-function a(){
-    for(var i=0; i<10; i++){
-        for(var j=0; j<10; j++){
-            console.log(i,j);
+function a() {
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            console.log(i, j);
         }
-        console.log(j,i);
+        console.log(j, i);
     }
 }
 //Prediction:
@@ -116,7 +115,8 @@ function a(){
 
 
 var z = 10;
-function a(){
+
+function a() {
     var z = 15;
     console.log(z);
 }
@@ -126,7 +126,8 @@ console.log(z);
 
 
 var z = 10;
-function a(){
+
+function a() {
     var z = 15;
     console.log(z);
 }
@@ -138,7 +139,8 @@ console.log(z);
 
 
 var z = 10;
-function a(){
+
+function a() {
     var z = 15;
     console.log(z);
     return z;
@@ -147,4 +149,3 @@ z = a();
 console.log(z);
 // //Prediction: z=10; z=15
 // //Output: 15, 15
-        
