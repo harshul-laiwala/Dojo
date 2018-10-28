7e2c523fe689791661c9f061a39c7769eba73b94
harshul-laiwala <harshul.laiwala@gmail.com>
2018-10-28 10:16:16 -0700

    blogs erd

--- WebFundAssignments-master/3.Javascript/IntermediateFoundation/IntermediateFoundation.js
+++ WebFundAssignments-master/3.Javascript/IntermediateFoundation/IntermediateFoundation.js
@@ -1,6 +1,25 @@
 // 1.Sigma - Implement function sigma(num) that given a number, returns the sum of all positive integers up to number (inclusive).  Ex: sigma(3) = 6 (or 1+2+3); sigma(5) = 15 (or 1+2+3+4+5).
 
+function Sigma(num){
+    var sum=0;
+    for(i=0;i<=num;i++){
+        sum+=i;
+    }
+    return sum;
+}
+console.log(Sigma(3));
+
 // 2.Factorial - Write a function factorial(num) that, given a number, returns the product (multiplication) of all positive integers from 1 up to number (inclusive).  For example, factorial(3) = 6 (or 1*2*3); factorial(5) = 120 (or 1*2*3*4*5).
+function factorial(num){
+    var result=1;
+    for(i=1;i<=num;i++){
+        result= i*result;
+        // console.log("i value is: " +i);
+        // console.log("result value is: " +result);
+    }
+    return result;
+}
+console.log(factorial(3));
 
 // 3.Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).  Do this without using recursion first.  If you don't know what a recursion is yet, don't worry as we'll be introducing this concept in Part 2 of this assignment.
 
